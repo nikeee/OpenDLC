7027408b473f6f3fb1ca4386f8010172ac5c9c90
Niklas Mollenhauer <nikeee@outlook.com>
2015-04-03 12:29:31 +0200

    Remove usings

--- src/OpenDLC/OpenDLC/CcfContainer.cs
+++ src/OpenDLC/OpenDLC/CcfContainer.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
