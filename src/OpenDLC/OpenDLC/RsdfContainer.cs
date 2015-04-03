3ea6c3d9a3fc41499c6e686a9c9305284c6b307a
Niklas Mollenhauer <nikeee@outlook.com>
2015-04-03 13:19:36 +0200

    Move RSDF-Specific stuff to own files

--- src/OpenDLC/OpenDLC/RsdfContainer.cs
+++ src/OpenDLC/OpenDLC/RsdfContainer.cs
@@ -10,19 +10,6 @@ namespace OpenDLC
 {
     public class RsdfContainer : DownloadContainer<RsdfEntry>
     {
-        private static readonly byte[] Key = { 0x8C, 0x35, 0x19, 0x2D, 0x96, 0x4D, 0xC3, 0x18, 0x2C, 0x6F, 0x84, 0xF3, 0x25, 0x22, 0x39, 0xEB, 0x4A, 0x32, 0x0D, 0x25, 0x00, 0x00, 0x00, 0x00 };
-
-        private static readonly byte[] IV = { 0xA3, 0xD5, 0xA3, 0x3C, 0xB9, 0x5A, 0xC1, 0xF5, 0xCB, 0xDB, 0x1A, 0xD2, 0x5C, 0xB0, 0xA7, 0xAA };
-
-        private static readonly Rijndael Rijndael = new RijndaelManaged
-        {
-            Padding = PaddingMode.None,
-            FeedbackSize = 8,
-            Mode = CipherMode.CFB,
-            BlockSize = 128,
-            KeySize = 128
-        };
-
         public static RsdfContainer FromFile(string fileName)
         {
             if (string.IsNullOrEmpty(fileName))
@@ -122,7 +109,7 @@ namespace OpenDLC
 
         public string SaveAsString()
         {
-            using (var enc = Rijndael.CreateEncryptor(Key, IV))
+            using (var enc = RsdfFormat.Algorithm.CreateEncryptor(RsdfFormat.Key, RsdfFormat.IV))
             {
                 var lines = this.Select(entry => entry.Url).ToArray();
 
@@ -165,7 +152,7 @@ namespace OpenDLC
 
             var container = new RsdfContainer();
 
-            using (var dec = Rijndael.CreateDecryptor(Key, IV))
+            using (var dec = RsdfFormat.Algorithm.CreateDecryptor(RsdfFormat.Key, RsdfFormat.IV))
             {
                 for (int i = 0; i < lines.Length; ++i)
                 {
