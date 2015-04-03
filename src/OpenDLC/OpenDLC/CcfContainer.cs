8a7ca7ac45f1ddd2a9262b681d9420e30616e544
Niklas Mollenhauer <nikeee@outlook.com>
2015-04-03 12:28:19 +0200

    Move static stuff to own file

--- src/OpenDLC/OpenDLC/CcfContainer.cs
+++ src/OpenDLC/OpenDLC/CcfContainer.cs
@@ -14,28 +14,7 @@ namespace OpenDLC
     {
         public Version Version { get; private set; }
 
-        #region crypto
-
-        private static readonly ReadOnlyCollection<byte[]> Keys = new ReadOnlyCollection<byte[]>(new[]
-        {
-            new byte[] {0x5F, 0x67, 0x9C, 0x00, 0x54, 0x87, 0x37, 0xE1, 0x20, 0xE6, 0x51, 0x8A, 0x98, 0x1B, 0xD0, 0xBA, 0x11, 0xAF, 0x5C, 0x71, 0x9E, 0x97, 0x50, 0x29, 0x83, 0xAD, 0x6A, 0xA3, 0x8E, 0xD7, 0x21, 0xC3},
-            new byte[] {0x17, 0x1B, 0xF8, 0xE3, 0x4C, 0x3D, 0x0C, 0x0C, 0x26, 0x93, 0xFD, 0xD2, 0xB0, 0x80, 0x42, 0x3A, 0x5B, 0x98, 0xF4, 0xD0, 0x28, 0xA0, 0xAF, 0x4D, 0x82, 0xA3, 0x85, 0xD8, 0x37, 0xA8, 0xF9, 0x5F},
-            new byte[] {0x02, 0x69, 0x00, 0xE9, 0x77, 0xC6, 0x40, 0x24, 0x42, 0xB6, 0x61, 0x32, 0x9C, 0xFE, 0x62, 0xD6, 0xED, 0x21, 0xBD, 0xEB, 0x0C, 0xD6, 0x32, 0x13, 0x18, 0xA8, 0xED, 0xC7, 0xBC, 0x5A, 0x6C, 0x86}
-        });
-        private static readonly ReadOnlyCollection<byte[]> IVs = new ReadOnlyCollection<byte[]>(new[]
-        {
-            new byte[] {0xE3, 0xD1, 0x53, 0xAD, 0x60, 0x9E, 0xF7, 0x35, 0x8D, 0x66, 0x68, 0x41, 0x80, 0xC7, 0x33, 0x1A},
-            new byte[] {0x9F, 0xE9, 0x5F, 0xFF, 0x7C, 0xA4, 0xFC, 0x0F, 0xCE, 0xF2, 0x5E, 0x4F, 0x74, 0x44, 0xAE, 0x67},
-            new byte[] {0x8C, 0xE1, 0x17, 0x3E, 0xBA, 0xD7, 0x6E, 0x08, 0x58, 0x4B, 0x94, 0x57, 0x39, 0x26, 0x23, 0x1E}
-        });
-        private static readonly ReadOnlyCollection<Version> Versions = new ReadOnlyCollection<Version>(new[]
-        {
-            new Version(1, 0),
-            new Version(0, 8),
-            new Version(0, 7)
-        });
-
-        #endregion
+        #region from*
 
         public static CcfContainer FromFile(string fileName)
         {
@@ -46,16 +25,6 @@ namespace OpenDLC
             return FromBuffer(buffer);
         }
 
-#if FEATURE_TAP
-        public static async Task<CcfContainer> FromFileAsync(string fileName)
-        {
-            if (string.IsNullOrEmpty(fileName))
-                throw new ArgumentNullException("fileName"); // TODO: nameof(fileName)
-
-            using (var f = File.OpenRead(fileName))
-                return await FromStreamAsync(f).ConfigureAwait(false);
-        }
-#endif
         public static CcfContainer FromStream(Stream stream)
         {
             if (stream == null)
@@ -73,7 +42,20 @@ namespace OpenDLC
             }
         }
 
+        #endregion
+        #region from*async
+
 #if FEATURE_TAP
+
+        public static async Task<CcfContainer> FromFileAsync(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentNullException("fileName"); // TODO: nameof(fileName)
+
+            using (var f = File.OpenRead(fileName))
+                return await FromStreamAsync(f).ConfigureAwait(false);
+        }
+
         public static async Task<CcfContainer> FromStreamAsync(Stream stream)
         {
             if (stream == null)
@@ -91,6 +73,9 @@ namespace OpenDLC
             }
         }
 #endif
+
+        #endregion
+
         public static CcfContainer FromBuffer(byte[] buffer)
         {
             if (buffer == null)
@@ -123,15 +108,6 @@ namespace OpenDLC
         }
 
 
-        private static RijndaelManaged CreateRijndael()
-        {
-            return new RijndaelManaged
-            {
-                Mode = CipherMode.CBC,
-                Padding = PaddingMode.Zeros
-            };
-        }
-
         public override void SaveToStream(Stream stream)
         {
             if (stream == null)
@@ -157,10 +133,10 @@ namespace OpenDLC
             const int version10KeyIndex = 0;
 
             var xmlDataBytes = SerializeToXml();
-            using (var rij = CreateRijndael())
+            using (var rij = CcfFormat.CreateSymmetricAlgorithm())
             {
-                rij.IV = IVs[version10KeyIndex];
-                rij.Key = Keys[version10KeyIndex];
+                rij.IV = CcfFormat.IVs[version10KeyIndex];
+                rij.Key = CcfFormat.Keys[version10KeyIndex];
                 using (var enc = rij.CreateEncryptor())
                 {
                     var outputResized = enc.TransformFinalBlock(xmlDataBytes, 0, xmlDataBytes.Length);
@@ -177,14 +153,14 @@ namespace OpenDLC
             Debug.Assert(data != null);
             Debug.Assert(data.Length > 0);
 
-            using (var rij = CreateRijndael())
+            using (var rij = CcfFormat.CreateSymmetricAlgorithm())
             {
                 var output = new byte[4096];
                 // There is more than one key. Just try them out.
-                for (int i = 0; i < Keys.Count; ++i)
+                for (int i = 0; i < CcfFormat.Keys.Count; ++i)
                 {
-                    rij.IV = IVs[i];
-                    rij.Key = Keys[i];
+                    rij.IV = CcfFormat.IVs[i];
+                    rij.Key = CcfFormat.Keys[i];
                     using (var dec = rij.CreateDecryptor())
                     {
                         var written = dec.TransformBlock(data, 0, data.Length, output, 0);
@@ -206,7 +182,7 @@ namespace OpenDLC
 
                         if (IsValidContainerXml(xmlData))
                         {
-                            usedVersion = Versions[i];
+                            usedVersion = CcfFormat.Versions[i];
                             return xmlData;
                         }
                     }
@@ -223,6 +199,8 @@ namespace OpenDLC
             return data.Contains(Identifier, StringComparison.InvariantCultureIgnoreCase);
         }
 
+        #region serialization
+
         private static readonly XmlSerializer _serializer = new XmlSerializer(typeof(CryptLoadContainer));
 
         private static CryptLoadContainer SerializeFromXml(string xmlData)
@@ -236,13 +214,6 @@ namespace OpenDLC
             }
         }
 
-        private static XmlWriterSettings _writerSettings = new XmlWriterSettings
-        {
-            Encoding = new UTF8Encoding(false),
-            Indent = false,
-            OmitXmlDeclaration = true
-        };
-
         private byte[] SerializeToXml()
         {
             var serCon = CryptLoadContainer.FromCcfContainer(this);
@@ -251,11 +222,13 @@ namespace OpenDLC
                 XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
                 namespaces.Add(string.Empty, string.Empty); // No namespaces because the others don't do it as well
 
-                using (var ww = XmlWriter.Create(ms, _writerSettings))
+                using (var ww = XmlWriter.Create(ms, CcfFormat.WriterSettings))
                     _serializer.Serialize(ww, serCon, namespaces);
 
                 return ms.ToArray();
             }
         }
+
+        #endregion
     }
 }
